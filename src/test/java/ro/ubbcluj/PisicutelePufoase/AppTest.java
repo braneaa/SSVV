a6cdb804a2890aef3b4768c0e9517e6695235fdd
Branea Alexandru <55286405+braneaa@users.noreply.github.com>
2021-04-17 15:21:04 +0300

    lab3

--- src/test/java/ro/ubbcluj/PisicutelePufoase/AppTest.java
+++ src/test/java/ro/ubbcluj/PisicutelePufoase/AppTest.java
@@ -2,22 +2,28 @@ package ro.ubbcluj.PisicutelePufoase;
 
 import org.junit.Before;
 import org.junit.Test;
+import src.ro.ubb.PisicutelePufoase.Domain.TemaLab;
 import src.ro.ubb.PisicutelePufoase.Exceptions.ValidatorException;
 import src.ro.ubb.PisicutelePufoase.Service.TxtFileService.StudentService;
+import src.ro.ubb.PisicutelePufoase.Service.TxtFileService.TemaLabService;
 
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
 public class AppTest {
 
     TestBuilder testBuilder = new TestBuilder();
     StudentService service;
+    TemaLabService temaLabService;
     @Before
     public void before() throws IOException {
         testBuilder = new TestBuilder();
         service = testBuilder.getService();
+        temaLabService = testBuilder.getServiceAssignment();
     }
 
     @Test
@@ -54,10 +60,57 @@ public class AppTest {
         }
     }
 
+    @Test
+    public void test4(){
+        testAssignmentWithIdZero();
+        testAssignmentWithEmptyDescription();
+        testAssignmentWithInvalidDeadline();
+        testAssignmentWithInvalidPrimire();
+    }
+
+    private void testAssignmentWithInvalidPrimire() {
+        try {
+            temaLabService.add(testBuilder.getAssignmentWithInvalidPrimire());
+        } catch (Exception exception) {
+            assertEquals(1, (int) getAssignmentsCount());
+        }
+
+    }
+
+    private void testAssignmentWithInvalidDeadline() {
+        try {
+            temaLabService.add(testBuilder.getAssignmentWithInvalidDeadline());
+        } catch (Exception exception) {
+            assertEquals(1, (int) getAssignmentsCount());
+        }
+    }
+
+    private void testAssignmentWithIdZero() {try {
+        temaLabService.add(testBuilder.getAssignmentWithIdZero());
+    } catch (Exception exception) {
+        assertEquals(1, (int) getAssignmentsCount());
+    }
+    }
+
+    private void testAssignmentWithEmptyDescription() {
+        try {
+            temaLabService.add(testBuilder.getAssignmentWithEmptyDescription());
+        } catch (Exception exception) {
+            assertEquals(1, (int) getAssignmentsCount());
+        }
+    }
+
     private Integer getStudentsCount() {
         AtomicInteger count = new AtomicInteger();
-        service.getAll().forEach(student -> {
-            count.addAndGet(1);});
+        service.getAll().forEach(student -> count.addAndGet(1));
+        return count.get();
+    }
+
+    private Integer getAssignmentsCount() {
+        AtomicReference<Integer> count = new AtomicReference<>(0);
+
+        temaLabService.getAll().forEach(assignment -> count.getAndSet(count.get() + 1));
+
         return count.get();
     }
 
--- src/test/java/ro/ubbcluj/PisicutelePufoase/TestBuilder.java
+++ src/test/java/ro/ubbcluj/PisicutelePufoase/TestBuilder.java
@@ -2,9 +2,12 @@ package ro.ubbcluj.PisicutelePufoase;
 
 
 import src.ro.ubb.PisicutelePufoase.Repository.TxtFileRepository.StudentFileRepo;
+import src.ro.ubb.PisicutelePufoase.Repository.TxtFileRepository.TemaLabFileRepo;
 import src.ro.ubb.PisicutelePufoase.Repository.XMLFileRepository.StudentXMLRepo;
 import src.ro.ubb.PisicutelePufoase.Service.TxtFileService.StudentService;
+import src.ro.ubb.PisicutelePufoase.Service.TxtFileService.TemaLabService;
 import src.ro.ubb.PisicutelePufoase.Validator.StudentValidator;
+import src.ro.ubb.PisicutelePufoase.Validator.TemaLabValidator;
 
 import java.io.IOException;
 
@@ -15,6 +18,16 @@ public class TestBuilder {
     public static String DEFAULT_GROUP = "932";
     public static String DEFAULT_EMAIL = "email@emabil.ro";
     public static String DEFAULT_INDRUMATOR = "indrumator";
+
+
+    public static int ID_0 = 0;
+    public static String descriere = "Lorem ipsum";
+    public static int deadline = 4;
+    public static int primire = 1;
+    public static int id = 7;
+    public static String empty_description = null;
+    public static int invalid_deadline = 0;
+    public static int invalid_primire = 0;
 
     //files
     public static String DEFAULT_STUDENT_XML_FILE = "testStudentXMLFile.xml";
@@ -55,5 +68,33 @@ public class TestBuilder {
 
     public StudentService getService() throws IOException {
         return new StudentService(getStudentFileRepo());
+    }
+
+    public String[] getAssignmentWithIdZero() {
+        return new String[]{String.valueOf(ID_0), descriere, String.valueOf(deadline), String.valueOf(primire)};
+    }
+
+    public String[] getAssignmentWithEmptyDescription() {
+        return new String[]{String.valueOf(id), empty_description, String.valueOf(deadline), String.valueOf(primire)};
+    }
+
+    public String[] getAssignmentWithInvalidDeadline() {
+        return new String[]{String.valueOf(id), descriere, String.valueOf(invalid_deadline), String.valueOf(primire)};
+    }
+
+    public String[] getAssignmentWithInvalidPrimire() {
+        return new String[]{String.valueOf(id), descriere, String.valueOf(deadline), String.valueOf(invalid_primire)};
+    }
+
+    public TemaLabService getServiceAssignment() throws IOException {
+        return new TemaLabService(getAssignmentFileRepo());
+    }
+
+    private TemaLabValidator getAssignmentValidator() {
+        return new TemaLabValidator();
+    }
+
+    private TemaLabFileRepo getAssignmentFileRepo() throws IOException {
+        return new TemaLabFileRepo(null ,getAssignmentValidator());
     }
 }
